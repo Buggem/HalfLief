27c5215b72b2882c9ef6b29b4936b4a9b0ebb0aa
Max Parry <66670928+Buggem@users.noreply.github.com>
2026-03-14 16:41:08 +1100

    Update index.js

--- watch/index.js
+++ watch/index.js
@@ -1,10 +1,19 @@
 window.vids = {
   "sMtckj6gnbg": {
     "title":  "Are these the world's most crispy fries? Let's find out.",
+    "author": "fryguy"
     "source": "/watch/fries_hd.mp4",
     "views": 561905,
     "votes": 35695,
     "comments": 564
+  }
+  "eP2JnstsOWY": {
+    "title": "watch me smoke for 3 seconds",
+    "author": "allool",
+    "source": "https://encrypted-vtbn0.gstatic.com/video?q=tbn:ANd9GcTJr9BsRJl6A7W9h9jOYGNP_ZFesTq_-Rwb1A",
+    "views": 65,
+    "votes": 10,
+    "comments": 0
   }
 };
 window.numbrr = (no) => {
